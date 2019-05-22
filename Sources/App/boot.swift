b15e475a6732aec3edefa1c365a5ddb438b2efb8
cweinberger <christian.weinberger@me.com>
2019-05-22 06:29:04 +0200

    Add ContainerInfo and gcr-example route

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,16 @@
 import Vapor
 
+internal struct ContainerInfo: Content {
+    static let date: Date = Date()
+    static let containerID: Int = Int.random(in: 0...Int.max)
+
+    let creationDate = ContainerInfo.date
+    let currentDate = Date()
+    let uuid = ContainerInfo.containerID
+}
+
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
+    _ = ContainerInfo()
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -17,4 +17,8 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+
+    router.get("gcr-example") { req in
+        return ContainerInfo()
+    }
 }
